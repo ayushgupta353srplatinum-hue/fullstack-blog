04e004054b52ad85c292ed92cb862f2906e42dcd
Ayush Gupta <ayushgupta353srplatinum@gmail.com>
2026-03-26 18:36:30 +0530

    Add files via upload

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,31 @@
+import { useEffect, useState } from "react";
+import API from "../api";
+import PostForm from "../components/PostForm";
+import PostCard from "../components/PostCard";
 
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const res = await API.get("/");
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog App</h1>
+
+      <PostForm fetchPosts={fetchPosts} />
+
+      {posts.map((post) => (
+        <PostCard key={post._id} post={post} fetchPosts={fetchPosts} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
