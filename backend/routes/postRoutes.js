2e5d4c21622680fa9ae7bfcb73f4526ff1e67a2d
Ayush Gupta <ayushgupta353srplatinum@gmail.com>
2026-03-26 18:23:13 +0530

    Add files via upload

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { createPost, getPosts, deletePost } from "../controllers/postController.js";
+import upload from "../config/multer.js";
 
+const router = express.Router();
+
+router.get("/", getPosts);
+router.post("/", upload.single("image"), createPost);
+router.delete("/:id", deletePost);
+
+export default router;
