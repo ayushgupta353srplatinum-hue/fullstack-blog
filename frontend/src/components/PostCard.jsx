a7551a6f69ee071b00e785367eebfc4debdf77c8
Ayush Gupta <ayushgupta353srplatinum@gmail.com>
2026-03-26 18:35:36 +0530

    Add files via upload

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,1 +1,21 @@
+import API from "../api";
 
+const PostCard = ({ post, fetchPosts }) => {
+  const handleDelete = async () => {
+    await API.delete(`/${post._id}`);
+    fetchPosts();
+  };
+
+  return (
+    <div>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+
+      {post.image && <img src={post.image} width="200" />}
+
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
+
+export default PostCard;
---
+++ frontend/src/components/PostForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+
+const API = "http://localhost:5000/api/posts";
+
+const PostForm = ({ fetchPosts }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    if (image) formData.append("image", image);
+
+    await axios.post(API, formData);
+
+    setTitle("");
+    setContent("");
+    setImage(null);
+
+    fetchPosts();
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        placeholder="Enter Title..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <textarea
+        placeholder="Write content..."
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+
+      <input
+        type="file"
+        onChange={(e) => setImage(e.target.files[0])}
+      />
+
+      <button>Create Post </button>
+    </form>
+  );
+};
+
+export default PostForm;
