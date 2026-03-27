b38a859e48b3730e81e05eb9cc375e6bf04c53ac
Ayush Gupta <ayushgupta353srplatinum@gmail.com>
2026-03-27 14:59:01 +0530

    Update PostForm.jsx

--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API = "https://fullstack-blog-2-s0en.onrender.com/api/posts";
+const API = "http://localhost:5000/api/posts";
 
 const PostForm = ({ fetchPosts }) => {
   const [title, setTitle] = useState("");
