bc5cba0e30079210d8fab5e546e33d4684dc2505
Ayush Gupta <ayushgupta353srplatinum@gmail.com>
2026-03-29 21:35:58 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import PostForm from "./components/PostForm";
 import "./index.css";
 
-const API = `${import.meta.env.VITE_API_URL}/api/posts`;
+const API = "http://localhost:5000/api/posts";
 
 function App() {
   const [posts, setPosts] = useState([]);
